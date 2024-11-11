0a5a106d96c580b421626aeb46f6186597ed6363
vyhoang1 <vh173@georgetown.edu>
2024-11-11 17:48:17 -0500

    Configured pytest

---
+++ app/__init__.py
@@ -0,0 +1,1 @@
+# the init file helps us import code from the directory where it exists which in this case is the "app" directory 
---
+++ test/rps_test.py
@@ -0,0 +1,19 @@
+
+# this is the "test/rps_test.py" file...
+
+from app.rps import determine_outcome
+
+def test_winners():
+    # tests for all edge cases:
+
+    assert determine_outcome(u="rock", c="rock") == "TIE GAME"
+    assert determine_outcome(u="rock", c="paper") == "COMPUTER WINS"
+    assert determine_outcome(u="rock", c="scissors") == "USER WINS"
+
+    assert determine_outcome(u="paper", c="rock") == "USER WINS"
+    assert determine_outcome(u="paper", c="paper") == "TIE GAME"
+    assert determine_outcome(u="paper", c="scissors") == "COMPUTER WINS"
+
+    assert determine_outcome(u="scissors", c="scissors") == "TIE GAME"
+    assert determine_outcome(u="scissors", c="paper") == "USER WINS"
+    assert determine_outcome(u="scissors", c="rock") == "COMPUTER WINS"
