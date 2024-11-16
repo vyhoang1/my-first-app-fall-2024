db567ae05b493906b940586510133e99677329c8
vyhoang1 <vh173@georgetown.edu>
2024-11-15 19:37:53 -0500

    Commit 11

--- app/stocks.py
+++ app/stocks.py
@@ -8,6 +8,9 @@ from plotly.express import line
 
 from app.alpha_service import API_KEY
 
+def format_usd(my_price):
+    return f"${float(my_price):,.2f}"
+
 def fetch_stocks_csv(symbol):
     request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&outputsize=full&datatype=csv"
     df = read_csv(request_url)
